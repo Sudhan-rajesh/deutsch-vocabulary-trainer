67c5b2c58effe4dd9e08698832ba9b99a686dc11
Sudhan-rajesh <sudhanvrajesh@gmail.com>
2026-04-22 18:23:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def main():
+    options()
+
+def options():
+    while True:
+        print("Vocabulary Trainer")
+        print("Press 1 to add new word")
+        print("Press 2 to practice vocabulary")
+        print("Press 3 to quit")
+
+        choice = int(input("Enter your choice (1-3): "))
+
+        if choice == 1:
+            ...
+        elif choice == 2:
+            ...
+        elif choice == 3:
+            ...
+        else:
+            print("Invalid choice, Please try again")
+        
+
+if __name__=="__main__":
+    main()
