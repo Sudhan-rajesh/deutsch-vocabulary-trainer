1530bceb6d9590bceb8ff66c1c9fa1851bca3438
Sudhan-rajesh <sudhanvrajesh@gmail.com>
2026-04-24 21:40:53 +0200

    build 24.04

---
+++ functions/add_word.py
@@ -0,0 +1,12 @@
+def pressed1():
+
+    import csv
+    english = input("Enter english word: ")
+    german = input("Enter the german translation: ")
+
+    with open("vocabulary/vocabulary.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["english","german"])
+        writer.writerow({"english":english,"german":german})
+        print("vocabulary list updated successfully!")
+        print(f"Last added word is {english} which means {german}")
+            
--- functions/menu.py
+++ functions/menu.py
@@ -1,18 +1,24 @@
+from .add_word import pressed1
+
 def show_menu():
     while True:
         print("Vocabulary Trainer")
         print("Press 1 to add new word")
         print("Press 2 to practice vocabulary")
-        print("Press 3 to quit")
+        print("Press 3 to show your vocabulary list")
+        print("Press 4 to exit")
 
-        choice = int(input("Enter your choice (1-3): "))
+        choice = int(input("Enter your choice (1-4): "))
 
         if choice == 1:
-            ...
+            pressed1()
         elif choice == 2:
             ...
         elif choice == 3:
             ...
+        elif choice == 4:
+            print("Auf Wiedersehen")
+            break
         else:
             print("Invalid choice, Please try again")
             
